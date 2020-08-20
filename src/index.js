a0f5e73c3918c51c34cd0355a339131704ca049b
JW-Ellis <jwilliamellis@gmail.com>
2020-08-19 21:04:47 -0500

    create baseline demo

--- src/index.js
+++ src/index.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const SuperheroDropdown = () => {
+  const [items, setItems] = React.useState([]);
+  const [value, setValue] = useState("Luke Skywalker");
+  React.useEffect(() => {
+    //pulls json data
+    async function getHeroes() {
+      const response = await fetch("http://swapi.dev/api/people/?page=2");
+      const body = await response.json();
+      //assigns fetched json data to object
+      setItems(
+        body.results.map(({ name, eye_color }) => ({
+          label: name,
+          value: name,
+          eye: eye_color,
+        }))
+      );
+    }
+
+    getHeroes();
+  }, []);
+
+  return (
+    <div>
+      {/* dropdown that sets value  */}
+      <select value={value} onChange={(e) => setValue(e.currentTarget.value)}>
+        {items.map(({ label, value }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      <div>
+        {/* filters based on value */}
+        {items
+          .filter((item) => item.value === value)
+          .map((filteredPerson) => (
+            <li>{filteredPerson.eye}</li>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <SuperheroDropdown />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
